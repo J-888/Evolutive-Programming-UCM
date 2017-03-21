05d80ee26d6ebcf39ba38c131e4d28c1a015065e
Usuario Local <Usuario Local>
2017-03-21 15:55:48 +0100

    Añadida codificacion binaria de Grey (aun no empleada) para posibles futuras implementaciones

--- src/util/Utiles.java
+++ src/util/Utiles.java
@@ -15,7 +15,32 @@ public class Utiles {
 	}
 	
 	public static int bin_dec(String n){
+		//n = grayToBinary(n); 	//descomentar para emplear codificacion binaria de grey
 		return Integer.parseInt(n, 2);
 	}
+	
+	public static String grayToBinary(String gray){
+	    String binary  = "";
+	 
+	    //  MSB of binary code is same as gray code
+	    binary += gray.charAt(0);	 
+	 
+	    // Compute remaining bits
+	    for (int i = 1; i < gray.length(); i++){
+	        // If current bit is 0, concatenate previous bit
+	        if (gray.charAt(i) == '0')
+	            binary += binary.charAt(i - 1);
+	 
+	        // Else, concatenate invert of previous bit
+	        else
+	            binary += flipBinary(binary.charAt(i - 1));
+	    }
+	    return binary;
+	}
+	
+	public static char flipBinary(char c) { 
+		return (c == '0')? '1': '0';
+	}
+	   
 	
 }
