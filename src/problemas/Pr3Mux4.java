36c3f2385a59f11ae81e1e97f3a8bd1810bae0ec
Usuario Local <Usuario Local>
2017-05-10 16:43:22 +0200

    Empezada implementacion de la generacion de poblacion inicial

--- src/problemas/Pr3Mux4.java
+++ src/problemas/Pr3Mux4.java
@@ -1,14 +1,30 @@
 package problemas;
 
+import geneticos.IndividuoPG;
+import geneticos.IndividuoStd;
+import geneticos.TipoCromosoma;
+import geneticos.cromosomas.CromosomaPG;
+import geneticos.cromosomas.CromosomaPermInt;
+
 import javax.swing.JFrame;
+
 import operadores.cruce.FuncionCruce;
 import operadores.fitness.FitnessPr3MuxN;
 import operadores.mutacion.FuncionMutacion;
 import operadores.seleccion.FuncionSeleccion;
+import util.pg.Node;
+import util.pg.UtilesPG;
 
 public class Pr3Mux4 extends Problema {
+	
+	String pobIniGenMethod = null;
+	int profundidadMax;
+	
 	public Pr3Mux4(FuncionCruce funcCruz, FuncionMutacion funcMuta, FuncionSeleccion funcSelec, double elite0to1, int numGenerations, int tamPob, JFrame gui){
 		super(funcCruz, funcMuta, funcSelec, elite0to1, numGenerations, tamPob, 1, gui);
+		
+		//this.pobIniGenMethod = gui.getPobIniGenMethod();
+		//this.profundidadMax = gui.getProfundidadMax();
 		
 		this.minimizacion = false;
 		this.funcFit = new FitnessPr3MuxN(this.minimizacion);
@@ -19,15 +35,42 @@ public class Pr3Mux4 extends Problema {
 	
 	
 	public void generaPobIni() {
-		String pobIniGenMethod = null;//gui.getPobIniGenMethod();
 		if(pobIniGenMethod.equals("Creciente")){
-			
+			for (int i = 0; i < tamPob; i++) {
+				Node crecNode = UtilesPG.genNodeCreciente(profundidadMax);
+				CromosomaPG cromCrec = new CromosomaPG(crecNode);
+				IndividuoPG indCrec = new IndividuoPG(cromCrec);
+				poblacion.add(indCrec);
+			}
 		}
-		else if(pobIniGenMethod.equals("Completo")){
-			
+		else if(pobIniGenMethod.equals("Completa")){
+			for (int i = 0; i < tamPob; i++) {
+				Node comNode = UtilesPG.genNodeCompleta(profundidadMax);
+				CromosomaPG cromCom = new CromosomaPG(comNode);
+				IndividuoPG indCom = new IndividuoPG(cromCom);
+				poblacion.add(indCom);
+			}
 		}
-		else if(pobIniGenMethod.equals("RampedAndHalf")){
+		else if(pobIniGenMethod.equals("RampedAndHalf")){	//TODO: comprobar
+
+			int numGrupos = profundidadMax - 1;
 			
+			for(int i = 0; i < numGrupos; i++){
+				for (int j = 0; j < tamPob; j++) {
+					if (j%0 == 0){
+						Node crecNode = UtilesPG.genNodeCreciente(i+2);
+						CromosomaPG cromCrec = new CromosomaPG(crecNode);
+						IndividuoPG indCrec = new IndividuoPG(cromCrec);
+						poblacion.add(indCrec);
+					}		
+					else{
+						Node comNode = UtilesPG.genNodeCompleta(i+2);
+						CromosomaPG cromCom = new CromosomaPG(comNode);
+						IndividuoPG indCom = new IndividuoPG(cromCom);
+						poblacion.add(indCom);
+					}
+				}
+			}
 		}
 		else{
 			System.err.println("Error en la selección de generación de pob ini");
--- src/util/pg/UtilesPG.java
+++ src/util/pg/UtilesPG.java
@@ -1,23 +1,13 @@
 package util.pg;
 
-import java.util.ArrayList;
-
-import geneticos.IndividuoStd;
-import geneticos.TipoCromosoma;
-import geneticos.cromosomas.CromosomaPermInt;
-
 public class UtilesPG {
-	public static ArrayList<IndividuoStd> genPobCreciente(){
-		/*CromosomaPG newCromo = new CromosomaPG(TipoCromosoma.CROMPG);
-		newCromo.randomizeCromosome(tolerancia);
-		
-		if(!mapping.contains(newCromo)){
-			mapping.add(newCromo);
-			IndividuoStd newInd = new IndividuoStd(newCromo);
-			poblacion.add(newInd);
-			i++;
-			
-		}*/
-	return null;
+	public static Node genNodeCreciente(int profundidadMax){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Node genNodeCompleta(int profundidadMax) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
